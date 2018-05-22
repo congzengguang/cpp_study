cc0102a54afe7c4b48a996d64c749933d20d74db
congzengguang <congzengguang@sina.cn>
2018-05-22 21:22:23 +0800

    cout 输出

---
+++ cpp/basic/cout_test.cpp
@@ -0,0 +1,14 @@
+//
+// Created by Administrators_ on 2018/5/22.
+//
+#include <iostream>
+#include <string>
+using namespace std;
+int main(){
+    cout << "要输出的字符串" << endl;
+    cout << "endl 是换行" << endl;
+    cout << "不换行";
+    cout << "换行" << "\n";
+    cout << "不换行";
+    return 0;
+}
