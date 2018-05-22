65b7c37c050917787da194ad55cf55eb8d213389
congzengguang <congzengguang@sina.cn>
2018-05-22 21:30:33 +0800

    cin 输入测试

---
+++ cpp/basic/cin_test.cpp
@@ -0,0 +1,13 @@
+//
+// Created by Administrators_ on 2018/5/22.
+//
+
+#include <iostream>
+#include <string>
+using namespace std;
+int main(){
+    string input;
+    cin >> input;
+    cout << "您输入的值：" << input << endl;
+    return 0;
+}
