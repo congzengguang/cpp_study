bccea0cc8176e9d4cecc6ef935d9e924c4ce3feb
congzengguang <congzengguang@sina.cn>
2018-05-23 20:38:04 +0800

    空指针，空指针在内存中的地址是0，0表示不可访问的内存；

---
+++ cpp/point/null_point.cpp
@@ -0,0 +1,12 @@
+//
+// Created by Administrators_ on 2018/5/23.
+//
+
+#include <iostream>
+#include <string>
+using namespace std;
+int main(){
+    int *p = NULL;
+    cout << p << endl;
+    return 0;
+}
