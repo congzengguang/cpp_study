b660e87f3ebb025a304fdd01bd8247b32cb1537d
congzengguang <congzengguang@sina.cn>
2018-05-24 22:02:25 +0800

    for循环

---
+++ cpp/basic/for_test.cpp
@@ -0,0 +1,12 @@
+//
+// Created by Administrators_ on 2018/5/24.
+//
+#include <iostream>
+#include <string>
+using namespace std;
+int main(){
+    for(int x =0;x<10;x++){
+        cout << x << endl;
+    }
+    return 0;
+}
